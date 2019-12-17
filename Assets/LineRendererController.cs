b3545c29da0c3a206c6e4cb78f014f0c975e71b1
Siarhei Pilat <spilat12@gmail.com>
2019-12-17 04:00:40 +0100

    first commit

---
+++ Assets/LineRendererController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererController : MonoBehaviour
+{
+    public LineRenderer LineRenderer;
+
+    public Transform[] Links;
+
+    void Start()
+    {
+        LineRenderer.positionCount = Links.Length;
+
+        for (int i = 0; i < Links.Length; i++)
+        {
+            LineRenderer.SetPosition(i, Links[i].position);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Links.Length; i++)
+        {
+            LineRenderer.SetPosition(i, Links[i].position);
+        }
+    }
+}
