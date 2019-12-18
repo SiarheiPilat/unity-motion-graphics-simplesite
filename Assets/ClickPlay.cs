aa1f9c41e5d6e5e1dc05919a168dda5710924f9a
Siarhei Pilat <spilat12@gmail.com>
2019-12-18 02:19:09 +0100

    i think that's final

---
+++ Assets/ClickPlay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ClickPlay : MonoBehaviour
+{
+    public AudioClip clip1, clip2;
+
+
+    public AudioSource AudioSource;
+
+    public void PlayClip1()
+    {
+        AudioSource.PlayOneShot(clip1);
+    }
+
+    public void PlayClip2()
+    {
+        AudioSource.PlayOneShot(clip2);
+    }
+}
